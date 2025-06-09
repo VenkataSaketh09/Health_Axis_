75471dc5d5b6952bd71a77c3e2f5dbfd15fa1f66
M-V-SAKETH <m.saketh@tachyonsecurity.eu>
2025-06-09 09:29:59 +0530

    complted navbar and header section

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import Home from "./pages/Home"
+import Doctors from "./pages/Doctors"
+import { Routes,Route } from "react-router-dom"
+import Login from "./pages/Login"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Myprofile from "./pages/Myprofile"
+import MyAppointments from "./pages/MyAppointments"
+import Appointment from "./pages/Appointement"
+import NavBar from "./components/NavBar"
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/doctors" element={<Doctors/>}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/my-profile" element={<Myprofile/>}></Route>
+        <Route path="/my-appointments" element={<MyAppointments/>}></Route>
+        <Route path="/appointement/:docId" element={<Appointment/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
