429893f5f1433c3e49e8b181d2728425d2364e89
M-V-SAKETH <m.saketh@tachyonsecurity.eu>
2025-06-19 15:48:58 +0530

    progress 23

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,6 +7,8 @@ const AdminContextProvider = ({ children }) => {
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
   const [doctors, setDoctors] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  const [dashData,setDashData]=useState(false);
   const backend_url = import.meta.env.VITE_BACKEND_URL;
   const getAllDoctors = async () => {
     try {
@@ -30,7 +32,7 @@ const AdminContextProvider = ({ children }) => {
     try {
       const { data } = await axios.post(
         backend_url + "/api/admin/change-availability",
-        {doctorId},
+        { doctorId },
         { headers: { aToken } }
       );
       if (data.success) {
@@ -43,6 +45,61 @@ const AdminContextProvider = ({ children }) => {
       toast.error(error.message);
     }
   };
+
+  const getAllAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backend_url + "/api/admin/appointments",
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        console.log(data.appointments);
+        setAppointments(data.appointments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backend_url + "/api/admin/cancel-appointment",
+        { appointmentId },
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllAppointments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const getDashData = async () => {
+    try {
+      const { data } = await axios.get(backend_url + "/api/admin/dashboard", {
+        headers: { aToken },
+      });
+      if(data.success){
+        setDashData(data.dashData)
+        console.log(data.dashData)
+      }
+      else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message)
+    }
+  }
+    
+
   return (
     <AdminContext.Provider
       value={{
@@ -52,6 +109,11 @@ const AdminContextProvider = ({ children }) => {
         doctors,
         getAllDoctors,
         changeAvailability,
+        appointments,
+        setAppointments,
+        getAllAppointments,
+        cancelAppointment,
+        dashData,getDashData
       }}
     >
       {children}
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,9 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 //API for adding doctor
 const addDoctor = async (req, res) => {
   try {
@@ -61,48 +63,115 @@ const addDoctor = async (req, res) => {
       degree,
       experience,
       about,
-      available:true,
+      available: true,
       fees,
       address,
       date: Date.now(),
     };
     const doctorUpload = new doctorModel(doctorData);
     await doctorUpload.save();
-    return res
-      .json({ success: true, message: "successfully uploaded data" });
+    return res.json({ success: true, message: "successfully uploaded data" });
   } catch (error) {
-    return  res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message });
   }
 };
 
-
-
-const loginAdmin=async(req,res)=>{
-  try{
-    const {email,password}=req.body
-  if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
-    const token=jwt.sign(email+password,process.env.JWT_SECRET)
-    return  res.json({success:true,token})
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      return res.json({ success: true, token });
+    } else {
+      return res.json({ success: false, message: "invalid credentials" });
+    }
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
-  else{
-    return  res.json({success:false,message:"invalid credentials"})
-  }
-  }
-  catch(error){
-    return  res.json({success:false,message:error.message})
-  }
-}
-
+};
 
 //ApI  to get All doctors for admin panel
-const AllDoctors=async(req,res)=>{
-  try{
-    const doctors=await doctorModel.find({}).select('-password')
-    res.json({success:true,doctors})
+const AllDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
   }
-  catch(error){
-    res.json({success:false,message:error.message})
-  }
-}
+};
 
-export { addDoctor,loginAdmin,AllDoctors };
+//API to get All Appointments
+const adminAppointments = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({});
+    // console.log(appointments)
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to cancel the appointment
+const appointmentCancel = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+    if (!appointmentId) {
+      return res.json({
+        success: false,
+        message: "Appointment ID is required",
+      });
+    }
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    if (!appointmentData) {
+      return res.json({ success: false, message: "Appointment not found" });
+    }
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+    //releasing doctor slot
+    const { doctorId, slotDate, slotTime } = appointmentData;
+    const doctorData = await doctorModel.findById(doctorId);
+    let slots_booked = doctorData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
+    await doctorModel.findByIdAndUpdate(doctorId, { slots_booked });
+    res.json({ success: true, message: "Appointment cancelled successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to get dashboard data for admin panel
+const adminDashboard = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({});
+    const users = await userModel.find({});
+    const appointments = await appointmentModel.find({});
+
+    const dashData = {
+      doctors: doctors.length,
+      appointments: appointments.length,
+      patients: users.length,
+      latest_appointments: appointments.reverse().slice(0, 5),
+    };
+    res.json({ success: true, dashData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export {
+  addDoctor,
+  loginAdmin,
+  AllDoctors,
+  adminAppointments,
+  appointmentCancel,
+  adminDashboard
+};
